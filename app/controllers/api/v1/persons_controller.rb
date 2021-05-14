881e4bcfd0f9434f28fb2de1c178847c189d4ffc
amc <amcmax212@gmail.com>
2021-05-14 13:36:46 +0300

    feat: wikipedia api article retrieval service

--- app/controllers/api/v1/persons_controller.rb
+++ app/controllers/api/v1/persons_controller.rb
@@ -3,8 +3,9 @@ module Api::V1
 
     def index
       @persons = Person.all
-      render json: @persons, include: ['articles']
-    end
 
+      render json: @persons, only: %i[id first_name second_name],
+                             include: { articles: { only: %i[id title url word_count source.name], include: { language: { only: %i[name value] } } } }
+    end
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,18 @@ class Person < ApplicationRecord
   validates :first_name, uniqueness: true
   validates :second_name, uniqueness: true
   validates :country, presence: true
-  has_many :articles, foreign_key: :person_id
+  has_many :articles, foreign_key: :person_id, dependent: :destroy
   belongs_to :country, class_name: 'Country', foreign_key: :country_id
+
+  after_create :retrieve_articles
+
+  private
+
+  def retrieve_articles
+    active_sources = Source.where(status: 'active')
+
+    active_sources.each do |source|
+      FetchArticlesService.new(source: source, person: self).perform
+    end
+  end
 end
